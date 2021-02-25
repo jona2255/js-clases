7ca80bcd2a0129a00cf02051e6080c43c76ea09c
Jonatan Sánchez <jona2255@gmail.com>
2021-02-25 18:32:08 +0100

    Terminados  los puntos  7 y 8

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-class Personajes {
+class Personaje {
   serie = "Juego de Tronos";
   nombre;
   familia;
@@ -17,8 +17,7 @@ class Personajes {
     this.estado = "muerto";
   }
 }
-
-class Rey extends Personajes {
+class Rey extends Personaje {
   añosReinado;
   frase = "Vais a morir todos";
   constructor(nombre, familia, edad, añosReinado) {
@@ -29,7 +28,7 @@ class Rey extends Personajes {
     super.comunicar(this.frase);
   }
 }
-class Luchador extends Personajes {
+class Luchador extends Personaje {
   armaUsada;
   destreza;
   frase = "Primero pego y luego pregunto";
@@ -43,7 +42,7 @@ class Luchador extends Personajes {
     super.comunicar(this.frase);
   }
 }
-class Asesor extends Personajes {
+class Asesor extends Personaje {
   asesora;
   frase = "No sé por qué, pero creo que voy a morir pronto";
   constructor(nombre, familia, edad, asesora) {
@@ -54,7 +53,7 @@ class Asesor extends Personajes {
     super.comunicar(this.frase);
   }
 }
-class Escudero extends Personajes {
+class Escudero extends Personaje {
   sirve;
   pelotismo;
   frase = "Soy un loser";
@@ -74,3 +73,49 @@ const jamie = new Luchador("Jamie", "Lannister", 35, "Espada", 6);
 const daenerys = new Luchador("Daenerys", "Targaryen", 22, "Dragones", 10);
 const tyrion = new Asesor("Tyrion", "Lannister", 30, daenerys);
 const bronn = new Escudero("Bronn", null, 23, jamie, 5);
+
+/*
+ la array del apartado 2 se tiene que llamar "personajes"
+*/
+
+
+
+matar(personajes);
+resumen(personajes);
+function matar(personajes) {
+  for (const personaje of personajes) {
+    if (personaje.nombre.toLowerCase() === "tyrion") {
+      personaje.morir()
+    } else if (personaje.nombre.toLowerCase() === "jamie") {
+      personaje.morir()
+    }
+  }
+}
+function resumen(listaPersonajes) {
+  const listaPersonajesFiltrada = [];
+  listaPersonajesFiltrada.push({
+    tipo: "Rey",
+    personajes: listaPersonajes
+      .filter(personaje => personaje instanceof Rey)
+      .sort((a, b) => a.edad - b.edad)
+  });
+  listaPersonajesFiltrada.push({
+    tipo: "Luchador",
+    personajes: listaPersonajes
+      .filter(personaje => personaje instanceof Luchador)
+      .sort((a, b) => a.edad - b.edad)
+  });
+  listaPersonajesFiltrada.push({
+    tipo: "Asesor",
+    personajes: listaPersonajes
+      .filter(personaje => personaje instanceof Asesor)
+      .sort((a, b) => a.edad - b.edad)
+  });
+  listaPersonajesFiltrada.push({
+    tipo: "Escudero",
+    personajes: listaPersonajes
+      .filter(personaje => personaje instanceof Escudero)
+      .sort((a, b) => a.edad - b.edad)
+  });
+  console.log(listaPersonajesFiltrada);
+}
